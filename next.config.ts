92d64c7d7a9d2240c688f0b8f6210ba6cd94e7d3
Mohd Sahil Siddiqui <mohdsahilsiddiqui36@gmail.com>
2025-12-25 23:55:03 +0530

    next config file

--- next.config.ts
+++ next.config.ts
@@ -13,7 +13,7 @@
 // export default nextConfig;
 
 /** @type {import('next').NextConfig} */
-const nextConfig = {
+const nextConfig: import("next").NextConfig = {
   images: {
     domains: [
       "api.microlink.io", // Microlink Image Preview
